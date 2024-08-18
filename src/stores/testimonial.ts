a42d1dfd4dffc6bb4e7a4e89ddc03172417a75af
DMikaia <faeldan03@gmail.com>
2024-08-18 18:13:01 +0300

    Feature: finished all the left sections

---
+++ src/stores/testimonial.ts
@@ -0,0 +1,20 @@
+export const testimonials = [
+  {
+    avatar: "/avatar/image-emily.jpg",
+    text: "We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.",
+    name: "Emily R.",
+    role: "Marketing Director",
+  },
+  {
+    avatar: "/avatar/image-thomas.jpg",
+    text: "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.",
+    name: "Thomas S.",
+    role: "Chief Operating Officer",
+  },
+  {
+    avatar: "/avatar/image-jennie.jpg",
+    text: "Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!",
+    name: "Jennie F.",
+    role: "Business Owner",
+  },
+];
